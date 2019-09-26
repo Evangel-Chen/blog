05df77ec7afd736ff59b40add661fe29aa12af1b
aodun <aodun.czp@alipay.com>
2019-09-27 01:58:18 +0800

    content

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ## About
-个人博客
+我的博客
+
+## Conten
+> Articles Read <br/>
+> Articles Write <br/>
+> Login <br/>
+> Link <br/>
+> About <br/>
+> 404 <br/>
+
--- blog.sql
+++ blog.sql
@@ -17,4 +17,4 @@ articleTime   DATE NOT NULL,
 articleClick INT DEFAULT 0
 );
 
-INSERT author VALUES(DEFAULT, 'node', 'e10adc3949ba59abbe56e057f20f883e');
+INSERT author VALUES(DEFAULT, 'KorsChen', '21c6eeb9b732c03526c2c4418ac31d34');
